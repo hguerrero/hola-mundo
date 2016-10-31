a4fb1b3216f1523f6452fa3b8845faf0d868046b
hguerrero <hguerrer@localhost>
2016-10-30 19:35:15 -0600

    changed to jar deployment

--- src/main/java/mx/com/nmp/cloud/test/HolaMundoApplication.java
+++ src/main/java/mx/com/nmp/cloud/test/HolaMundoApplication.java
@@ -6,13 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HolaMundoApplication extends SpringBootServletInitializer {
+public class HolaMundoApplication {
 
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HolaMundoApplication.class);
-    }
-	
 	public static void main(String[] args) {
 		SpringApplication.run(HolaMundoApplication.class, args);
 	}
